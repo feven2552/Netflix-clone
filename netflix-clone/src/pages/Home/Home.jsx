7b28dd91c6c0c57c971d3a9d234f06b258f7e9e2
FEVEN <feven.25yi@gmail.com>
2024-05-13 11:13:57 +0300

    Row

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/Header"
 import Footer from "../../components/Footer/Footer"
 import Banner from "../../components/Banner/Banner"
+import RowList from "../../components/Rows/RowList/RowList"
 export default function home(){
   return (
     <> 
        <Header/>
        <Banner/>
+       <RowList/>
       <Footer/>
     
      </>
